3fb1b1f41aa70a425e7dbc2aa6db81a23242780d
deekshact887-lab <deekshact887@gmail.com>
2026-06-10 16:30:51 +0530

    Fix syntax errors and add main function

---
+++ miniprtcode.c
@@ -0,0 +1,23 @@
+/*#include<stdio.h>
+#define rows 30
+#define columnes 30
+char canvas[rows][columns];
+void initializeCanvas()
+{
+  int i,j;
+  for(i=0;i<rows;i++)
+{
+for(j=0;j<columns;j++){
+canvas[i][j]='_';
+}
+}
+}
+void displayCanvas()
+{
+        int i,j;
+for(i=0;i<rows;i++){
+for*/
+#iclude<stdio.h>
+int main()
+printf("hello world");
+return 0;
